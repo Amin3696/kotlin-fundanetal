5d050b6af06e55923e57c1f923fedb0c3c7efde9
Amin Shahimian <mohammadamin.shahimian@sti-consulting.com>
2023-05-02 10:01:44 +0200

    variables practices

--- src/main/kotlin/kotlinFundamentals/variables.kt
+++ src/main/kotlin/kotlinFundamentals/variables.kt
@@ -1,5 +1,8 @@
 package kotlinFundamentals
 
+import kotlin.random.Random
+
+
 fun main() {
     // all variables in Kotlin should be defined with val or var
     //val is immutable variable and var is mutable variable
@@ -7,7 +10,7 @@ fun main() {
     //in Kotlin, it is not mandatory to define the type of variable,
     // it can recognize type of variable by value (type inference)
 
-    val name: String = "Amin"
+   /* val name: String = "Amin"
     val name1 = "Ali"
     println("name is $name and name1 is $name1")
 
@@ -21,12 +24,26 @@ fun main() {
     println(message = "Gross salary is $salary €,\ntaxes is ${salary * .25}€ \nnet salary is ${salary - (salary * .25)}€")
 
     //another way to have multiline variables is using """ operator
-    val multiline = """  
+    val multiline = """
         Gross : $salary
         taxes : ${salary * .25}€
         net   : ${'$'}{salary-(salary*.25)
-        
+
     """.trimIndent()
     println(message = multiline)
+
+    */
+
+    val randomNumbers = mutableSetOf<Int>()
+    while (randomNumbers.size < 5) {
+        randomNumbers.add((1..50).random())
+    }
+    println("random1-50: $randomNumbers")
+
+    val randomNumbers2 = mutableSetOf<Int>()
+    while (randomNumbers2.size < 2) {
+        randomNumbers2.add((1..12).random())
+    }
+    println("random1-12: $randomNumbers2")
 }
 
