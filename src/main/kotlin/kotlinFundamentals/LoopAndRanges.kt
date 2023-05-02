6d271f135f08cb31ee4a2143b486d44c49498c01
Amin Shahimian <mohammadamin.shahimian@sti-consulting.com>
2023-05-02 12:50:09 +0200

    while and do-while

--- src/main/kotlin/kotlinFundamentals/LoopAndRanges.kt
+++ src/main/kotlin/kotlinFundamentals/LoopAndRanges.kt
@@ -1,19 +1,38 @@
 package kotlinFundamentals
 
 fun main() {
-    val range = 1..100
-    for (i in range) {
-        println("$i")
-    }
+    /*    val range = 1..100
+        for (i in range) {
+            println("$i")
+        }
 
-    //reverse range
-    val reverseRange = 10 downTo 1
-    for (i in reverseRange) {
-        println("reverse range: $i")
-    }
+        //reverse range
+        val reverseRange = 10 downTo 1
+        for (i in reverseRange) {
+            println("reverse range: $i")
+        }
 
-    for (i in reverseRange step 2) {
-        println("reverse range with skip : $i")
-    }
+        for (i in reverseRange step 2) {
+            println("reverse range with skip : $i")
+        }*/
 
+    exporeWhile()
+    exploreDoWhile()
+}
+
+fun exploreDoWhile() {
+    var i=0
+    do {
+        println("i value is: $i")
+        i++
+    }
+    while (i<6)
+}
+
+fun exporeWhile() {
+    var x = 1
+    while (x < 5) {
+        println("value of x is: $x")
+        x++
+    }
 }
