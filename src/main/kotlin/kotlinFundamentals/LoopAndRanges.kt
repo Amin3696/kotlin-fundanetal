8eb9b373fda3b3bd6303ddc0037e080c7595deb2
Amin Shahimian <mohammadamin.shahimian@sti-consulting.com>
2023-05-02 11:27:57 +0200

    loop and ranges with step

--- src/main/kotlin/kotlinFundamentals/LoopAndRanges.kt
+++ src/main/kotlin/kotlinFundamentals/LoopAndRanges.kt
@@ -1,5 +1,19 @@
 package kotlinFundamentals
 
-fun main(){
+fun main() {
+    val range = 1..100
+    for (i in range) {
+        println("$i")
+    }
+
+    //reverse range
+    val reverseRange = 10 downTo 1
+    for (i in reverseRange) {
+        println("reverse range: $i")
+    }
+
+    for (i in reverseRange step 2) {
+        println("reverse range with skip : $i")
+    }
 
 }
